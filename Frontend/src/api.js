97002bc35899e58632c8bf237d12c0fa2a450b70
SARthak2217 <jainsarthak672@gmail.com>
2026-05-22 14:51:51 +0530

    deploy

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://readify-1-u98x.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://readify-odo7.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
